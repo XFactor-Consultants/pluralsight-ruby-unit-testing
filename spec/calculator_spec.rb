13c26cd933791829f40f265013e3c7deeb223d47
Henchworm <agop5134@gmail.com>
2023-11-13 21:42:07 -0700

    completes mod 3

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,17 +3,17 @@
 require './calculator'
 
 RSpec.describe Calculator do
+  let(:calc) { Calculator.new }
+
   describe '#add' do
     context 'when given positive numbers' do
       it 'returns the sum of two numbers' do
-        calc = Calculator.new
         result = calc.add(5, 3)
         expect(result).to eq(8)
       end
     end
     context 'when given negative numbers' do
       it 'returns the sum of two numbers' do
-        calc = Calculator.new
         result = calc.add(-5, -3)
         expect(result).to eq(-8)
       end
@@ -22,9 +22,37 @@ RSpec.describe Calculator do
   describe '#subtract' do
     context 'when given positive numbers' do
       it 'returns the difference of two numbers' do
-        calc = Calculator.new
-        result = calc.subtract(5, 1)
+        num1 = 5
+        num2 = 1
+        result = calc.subtract(num1, num2)
         expect(result).to eq(4)
+        expect(result).to be < num1
+      end
+    end
+  end
+  describe '#multiply' do
+    context 'when given two positive numbers' do
+      it 'returns the product of two numbers' do
+        expect(calc.multiply(2, 2)).to eq(4)
+      end
+    end
+  end
+  describe '#divide' do
+    context 'when given two positive numbers' do
+      it 'returns a float for nonzero division' do
+        expect(calc.divide(10, 3)).to be_a(Float)
+        expect(calc.divide(10, 3)).to be_within(0.01).of(3.33)
+      end
+      it 'returns a truthy value when not dividing by zero' do
+        expect(calc.divide(10, 1)).to be_truthy
+      end
+      it 'does not return nil' do
+        expect(calc.divide(10, 1)).not_to be_nil
+      end
+    end
+    context 'errors' do
+      it 'raises an error when dividing by zero' do
+        expect { calc.divide(10, 0) }.to raise_error(StandardError, 'Cannot Divide by 0')
       end
     end
   end
